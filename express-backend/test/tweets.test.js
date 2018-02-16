9324d4a4b31f3eb2aff9c3ce64753e62cbc66eb7
oliverpople <oliverpople@gmail.com>
2018-02-16 15:10:38 +0000

    Removed comments and tidied code.

--- express-backend/test/tweets.test.js
+++ express-backend/test/tweets.test.js
@@ -3,24 +3,23 @@ const nock = require('nock');
 const expect = require('chai').expect;
 const fs = require("fs");
 
-describe("Testing API with a mocked backend", function () {
+describe("Testing twitter API with a mock", function () {
+  var contents = fs.readFileSync('./mock_data/tweet.json');
+  var jsonContent = JSON.parse(contents);
+
+  beforeEach(() => {
+    nock('https://api.twitter.com')
+      .get('/1.1/statuses/user_timeline.json?screen_name=nodejs&count=20')
+      .reply(200, jsonContent);
+   });
 
   it("responds with json file response", function (done) {
-      //read the json file
-      var contents = fs.readFileSync('./mock_data/tweet.json');
-      //parse the contents and assign to a variable
-      var jsonContent = JSON.parse(contents);
-
-      nock('https://api.twitter.com')
-        .get('/1.1/statuses/user_timeline.json?screen_name=nodejs&count=20')
-        .reply(200, jsonContent);
-
-      request
-        .get('/1.1/statuses/user_timeline.json?screen_name=nodejs&count=20')
-        .end(function (err, res) {
-          expect(res.status).to.equal(200);
-          expect(res.body.user.screen_name).to.equal("twitterapi")
-          done();
-        });
+    request
+      .get('/1.1/statuses/user_timeline.json?screen_name=nodejs&count=20')
+      .end(function (err, res) {
+        expect(res.status).to.equal(200);
+        expect(res.body.user.screen_name).to.equal("twitterapi")
+        done();
+      });
     })
   });
